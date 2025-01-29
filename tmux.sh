72bd17c59add24b58f12381221793ca42d094f05
Nin0 <26253311+N-I-N-0@users.noreply.github.com>
2025-01-29 10:58:41 +0100

    Add files via upload

---
+++ tmux.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/N-I-N-0/SoftSec1.git ~/Nin0/
+cd ~/Nin0
+unzip -o ~/Nin0/plugins.zip -d ~/.tmux/plugins/
+cp ~/Nin0/.tmux.conf ~/.tmux.conf
+tmux source ~/.tmux.conf
